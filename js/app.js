d9c34e55c9e8bd7eb38e4dd6cd033f5efdf53760
Aarthy Ravi <aarthydravi@gmail.com>
2017-10-14 11:28:37 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,17 +2,61 @@ var initialStock = [
   {
     title: 'Google',
     symbol: 'GOOGL',
+    quan: 4,
+    stockPrice: 850,
+    total: 0,
   },
   {
     title: 'Apple',
     symbol: 'AAPL',
-  }
+    quan: 2,
+    stockPrice: 120,
+    total: 0,
+  },
 ];
+t = 0;
+var $total = $('#total');
+for (i=0;i<initialStock.length;i++) {
+  t += initialStock[i].quan * initialStock[i].stockPrice;
+}
+$total.append(t)
+for (j=0;j<initialStock.length;j++) {
+  var googleUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+ initialStock[j].symbol + '&apikey=MC0RBZLQPSIREYCD';
+
+  currentstock(function(data,data1) {
+    for (k=0;k<initialStock.length;k++) {
+      if (initialStock[k].symbol == data1){
+        currenttotal = 0
+        var $currenttotal = $('#info')
+        t1 = initialStock[k].quan * data
+      }
+    }
+    $currenttotal.append(t1)
+  });
+
+}
+  function currentstock (callback){
+    $.ajax({
+      url: googleUrl,
+      dataType: "json",
+      success: function( data ){
+        var stockdata = (data["Meta Data"]["2. Symbol"]);
+        var currentdate = new Date();
+        var date =  currentdate.getFullYear() + "-"
+                   + (currentdate.getMonth()+1)  + "-"
+                   + ("0"+(currentdate.getDate())).slice(-2)
+        var stockdata2 = (data["Time Series (Daily)"][date]["4. close"]);
+        if(typeof callback === "function") callback(stockdata2,stockdata);
+      }
+    });
+
+  }
+
 
 var stockInformation = function (data) {
   this.title = (data.title);
   this.symbol = (data.symbol);
-
+  this.total = (data.stockPrice * data.quan);
 };
 
 var ViewModel = function () {
@@ -27,7 +71,6 @@ var ViewModel = function () {
   this.changeStock = function (clickStock) {
     populate(clickStock);
   };
-
 }
 
 function populate(clickStock){
